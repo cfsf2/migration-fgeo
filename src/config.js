1380616a1d0984787c4a56ab9916a1aba15aee87
Lautaro Bruno <lautarobruno225@gmail.com>
2021-06-10 13:10:23 -0300

    config change

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   image_path_server: "https://farmageo2.s3.amazonaws.com/", //incluir "/"
-  apiFarmageo: "https://api.farmageo.com.ar",
+  apiFarmageo: "https://api.testingdev.ml",
   authFarmageo: "https://admin.farmageo.com.ar:8000",
   // apiFarmageo: "http://localhost:3010",
 };
