7d6b68ce3f6a215a89f953dec37faecccb8da843
Bruno Ramari <bjramariutn@hotmail.com>
2021-12-02 11:46:43 -0300

    listado de farmacias no muestra farmacias demo

--- src/redux/reducers/FarmaciasReducer.js
+++ src/redux/reducers/FarmaciasReducer.js
@@ -1,6 +1,6 @@
 const defaultState = {
   farmacias: [],
-  farms_cerca_cant: '...',
+  farms_cerca_cant: "...",
 };
 
 export default (state = defaultState, action) => {
@@ -9,10 +9,14 @@ export default (state = defaultState, action) => {
       return {
         ...state,
         farmacias: action.payload.filter((f) => {
-          return f.habilitado && f.perfil_farmageo != 'no_visible' && f.perfil_farmageo != 'indefinido'
+          return (
+            f.habilitado &&
+            f.perfil_farmageo != "no_visible" &&
+            f.perfil_farmageo != "indefinido" &&
+            f.perfil_farmageo != "demo"
+          );
         }),
       };
-
 
     default:
       return state;
