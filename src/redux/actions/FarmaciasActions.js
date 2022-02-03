1b11b8a44bb3ba34a66ed7977c788de3d34af93f
Bruno Ramari <bjramariutn@hotmail.com>
2022-02-03 09:18:47 -0300

    FarmaciasAction: cambio de email destino a  coordinador@farmageo.com.ar

--- src/redux/actions/FarmaciasActions.js
+++ src/redux/actions/FarmaciasActions.js
@@ -1,29 +1,27 @@
 import axios from "axios";
 import { apiFarmageo } from "../../config";
 
-import '../../helpers/requestParser';
-
-
+import "../../helpers/requestParser";
 
 export const GET_FARMACIAS = (token) => {
-    return (dispatch) => {
-        axios
-            .get(apiFarmageo + "/farmacias", {})
-            .then(function (response) {
-                dispatch({
-                    type: "GET_FARMACIAS",
-                    payload: response.data,
-                });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    };
+  return (dispatch) => {
+    axios
+      .get(apiFarmageo + "/farmacias", {})
+      .then(function (response) {
+        dispatch({
+          type: "GET_FARMACIAS",
+          payload: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 };
 
 export const SOLICITUD_REGISTRO_FARMACIA = (solicitud) => {
-    var emailFarmacia = solicitud.email;
-    var html = `
+  var emailFarmacia = solicitud.email;
+  var html = `
       <p>Farmacia:<b> ${solicitud.nombre}</b></p>
       <p>Farmacéutico: <b> ${solicitud.nombrefarmaceutico}</b></p>
       <p>Matrícula: <b> ${solicitud.matricula}</b></p>
@@ -33,19 +31,19 @@ export const SOLICITUD_REGISTRO_FARMACIA = (solicitud) => {
       <p>Email: <b> ${solicitud.email}</b></p>
       `;
 
-    return axios
-        .post(apiFarmageo + "/pedidos/email", {
-            destinatario: emailFarmacia + ";comercial@farmageo.com.ar",
-            asunto: "Solicitud de registro Farmageo",
-            html: html,
-        })
-        .then(function (response) {
-            console.log(response)
-            return true;
-        })
-        .catch(function (error) {
-            console.log(error);
-            alert('Ha ocurrido un error, intente por favor más tarde.')
-            return false
-        });
+  return axios
+    .post(apiFarmageo + "/pedidos/email", {
+      destinatario: emailFarmacia + ";coordinador@farmageo.com.ar",
+      asunto: "Solicitud de registro Farmageo",
+      html: html,
+    })
+    .then(function (response) {
+      console.log(response);
+      return true;
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert("Ha ocurrido un error, intente por favor más tarde.");
+      return false;
+    });
 };
