3b461233084defefec7c9b62fa3edafe72e9750c
Bruno Ramari <bjramariutn@hotmail.com>
2022-03-15 11:51:36 -0300

    config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   image_path_server: "https://testing-farmageo.s3.amazonaws.com/", //incluir "/"
-  apiFarmageo: "https://api.testingdev.ml",
+  apiFarmageo: "https://api.farmageo.com.ar",
   authFarmageo: "https://admin.farmageo.com.ar:8000",
   base: "https://app.farmageo.com.ar",
   //apiFarmageo: "http://localhost:3110",
