5f4d2a09f0cb857cfabaf4fbe61e97d9f3b1d1cc
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-07-19 13:03:21 -0300

    se agrego servicio panales pami

--- src/views/components/farmaciaPerfil/Servicios.js
+++ src/views/components/farmaciaPerfil/Servicios.js
@@ -1,13 +1,14 @@
-import grupo287 from "../../../assets/images/Grupo 287.png";
-import presion from "../../../assets/images/presion.png";
-import inyeccion from "../../../assets/images/inyeccion.png";
-import amarillos from "../../../assets/images/amarillos.png";
-import whatsapp from "../../../assets/images/whatsapp.png";
-import magistrales from "../../../assets/images/recetas-magistrales.png";
-import testcovid from "../../../assets/images/covidtest.png";
+import grupo287 from '../../../assets/images/Grupo 287.png';
+import presion from '../../../assets/images/presion.png';
+import inyeccion from '../../../assets/images/inyeccion.png';
+import amarillos from '../../../assets/images/amarillos.png';
+import whatsapp from '../../../assets/images/whatsapp.png';
+import magistrales from '../../../assets/images/recetas-magistrales.png';
+import testcovid from '../../../assets/images/covidtest.png';
+import panales_pami from '../../../assets/images/panales_pami.jpeg';
 
-import _ from "lodash";
-import { checkServicio } from "../../helpers/FarmaciaHelpers";
+import _ from 'lodash';
+import { checkServicio } from '../../helpers/FarmaciaHelpers';
 
 function Servicios(props) {
   const { servicios, wapp } = props;
@@ -27,20 +28,20 @@ function Servicios(props) {
             <div
               className="col-sm col-6 pt-4"
               style={
-                checkServicio("recetas-magistrales", servicios)
-                  ? { filter: "none", opacity: 1 }
-                  : { filter: "grayscale(100%)", opacity: 0.3 }
+                checkServicio('recetas-magistrales', servicios)
+                  ? { filter: 'none', opacity: 1 }
+                  : { filter: 'grayscale(100%)', opacity: 0.3 }
               }
             >
-              <img src={magistrales} className="icons-md" alt="" />{" "}
+              <img src={magistrales} className="icons-md" alt="" />{' '}
               <p>Recetas magistrales</p>
             </div>
             <div
               className="col-sm col-6 pt-4"
               style={
-                checkServicio("violeta", servicios)
-                  ? { filter: "none", opacity: 1 }
-                  : { filter: "grayscale(100%)", opacity: 0.3 }
+                checkServicio('violeta', servicios)
+                  ? { filter: 'none', opacity: 1 }
+                  : { filter: 'grayscale(100%)', opacity: 0.3 }
               }
             >
               <img src={grupo287} className="icons-lg" alt="" />
@@ -48,20 +49,20 @@ function Servicios(props) {
             <div
               className="col-sm col-6 pt-4"
               style={
-                checkServicio("presion", servicios)
-                  ? { filter: "none", opacity: 1 }
-                  : { filter: "grayscale(100%)", opacity: 0.3 }
+                checkServicio('presion', servicios)
+                  ? { filter: 'none', opacity: 1 }
+                  : { filter: 'grayscale(100%)', opacity: 0.3 }
               }
             >
-              <img src={presion} className="icons-md" alt="" />{" "}
+              <img src={presion} className="icons-md" alt="" />{' '}
               <p>Toma de presión</p>
             </div>
             <div
               className="col-sm col-6 pt-4"
               style={
-                checkServicio("amarillos", servicios)
-                  ? { filter: "none", opacity: 1 }
-                  : { filter: "grayscale(100%)", opacity: 0.3 }
+                checkServicio('amarillos', servicios)
+                  ? { filter: 'none', opacity: 1 }
+                  : { filter: 'grayscale(100%)', opacity: 0.3 }
               }
             >
               <img src={amarillos} className="icons-md" alt="" />
@@ -70,9 +71,9 @@ function Servicios(props) {
             <div
               className="col-sm col-6 pt-4"
               style={
-                checkServicio("inyectables", servicios)
-                  ? { filter: "none", opacity: 1 }
-                  : { filter: "grayscale(100%)", opacity: 0.3 }
+                checkServicio('inyectables', servicios)
+                  ? { filter: 'none', opacity: 1 }
+                  : { filter: 'grayscale(100%)', opacity: 0.3 }
               }
             >
               <img src={inyeccion} className="icons-md" alt="" />
@@ -81,25 +82,36 @@ function Servicios(props) {
             <div
               className="col-sm col-6 pt-4"
               style={
-                checkServicio("testcovid", servicios)
-                  ? { filter: "none", opacity: 1 }
-                  : { filter: "grayscale(100%)", opacity: 0.3 }
+                checkServicio('testcovid', servicios)
+                  ? { filter: 'none', opacity: 1 }
+                  : { filter: 'grayscale(100%)', opacity: 0.3 }
               }
             >
               <img src={testcovid} className="icons-md" alt="" />
               <p>Test de Covid</p>
             </div>
             <div
+              className="col-sm col-6 pt-4"
+              style={
+                checkServicio('pañalespami', servicios)
+                  ? { filter: 'none', opacity: 1 }
+                  : { filter: 'grayscale(100%)', opacity: 0.3 }
+              }
+            >
+              <img src={panales_pami} className="icons-md" alt="" />
+              <p>Pañales PAMI</p>
+            </div>
+            <div
               className="col-sm col-6 bg-verde p-3 pt-4"
               style={
-                checkServicio("whatsapp", servicios)
-                  ? { filter: "none", opacity: 1 }
-                  : { filter: "grayscale(100%)", opacity: 0.3 }
+                checkServicio('whatsapp', servicios)
+                  ? { filter: 'none', opacity: 1 }
+                  : { filter: 'grayscale(100%)', opacity: 0.3 }
               }
             >
               <a
                 href={
-                  checkServicio("whatsapp", servicios)
+                  checkServicio('whatsapp', servicios)
                     ? `https://api.whatsapp.com/send?phone=+549${wapp}`
                     : null
                 }
