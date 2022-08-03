881cb1d536976c3e3b69f59d4046097c4c6cb121
Bruno Ramari <bjramari@hotmail.com>
2022-08-03 14:00:46 -0300

    .env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   image_path_server: "https://sync-farmageo2.s3.amazonaws.com/", //incluir "/"
-  apiFarmageo: "https://api.farmageo.com.ar",
+  apiFarmageo: process.env.REACT_APP_FARMAGEO_API,
   authFarmageo: "https://admin.farmageo.com.ar:8000",
   base: "https://app.farmageo.com.ar",
   //apiFarmageo: "http://localhost:3110",
