6fcaebbdd8fcfa5199889e22d6a73d46d94624bd
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2023-01-17 11:14:18 -0300

    Fix App.js_: Route TerminosCondiciones

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ import './css/switch.css';
 import { base } from './config';
 
 // import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import {
-  BrowserRouter,
-  HashRouter,
-  Route,
-  Switch,
-  useLocation,
-} from 'react-router-dom';
-import { browserHistory } from 'react-router';
-
+import { Route, Switch, useLocation } from 'react-router-dom';
 import FooterHome from './views/components/footers/FooterHome';
 import axios from 'axios';
 
@@ -75,7 +67,7 @@ const GestorCampanas = React.lazy(() =>
   import('./views/components/gestorCampanas/GestorCampanas')
 );
 
-const TermYCondiciones = React.lazy(() =>
+const TerminosCondiciones = React.lazy(() =>
   import('./views/helpers/TerminosCondiciones')
 );
 
@@ -132,248 +124,246 @@ function App() {
       ) : null}
       <React.Suspense fallback={loading()}>
         <GestorCampanas />
-        <BrowserRouter>
-          <Switch>
-            <Route
-              exact
-              path="/mutual"
-              name="Mutual"
-              render={(props) => (
-                <>
-                  <NavPerfil
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <Mutual {...props} />
-                </>
-              )}
-            />
-            <Route
-              exact
-              path="/novedades/terminos-y-condiciones/"
-              name="Términos y condiciones"
-              render={(props) => <TermYCondiciones {...props} />}
-            />
-            <Route
-              path="/farmaciaperfil"
-              name="farmacia"
-              render={(props) => (
-                <>
-                  <NavPerfil
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <FarmaciaPerfil {...props} />
-                </>
-              )}
-            />
+        <Switch>
+          <Route
+            exact
+            path="/mutual"
+            name="Mutual"
+            render={(props) => (
+              <>
+                <NavPerfil
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <Mutual {...props} />
+              </>
+            )}
+          />
+          <Route
+            exact
+            path="/novedades/terminos-y-condiciones/"
+            name="Términos y condiciones"
+            render={(props) => <TerminosCondiciones {...props} />}
+          />
+          <Route
+            path="/farmaciaperfil"
+            name="farmacia"
+            render={(props) => (
+              <>
+                <NavPerfil
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <FarmaciaPerfil {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/farmprodcat"
-              name="farmprodcat"
-              render={(props) => (
-                <>
-                  <NavPerfil
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <FarmaciaProductosCat {...props} />
-                </>
-              )}
-            />
+          <Route
+            path="/farmprodcat"
+            name="farmprodcat"
+            render={(props) => (
+              <>
+                <NavPerfil
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <FarmaciaProductosCat {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/revisarpedido"
-              name="revisarpedido"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <RevisarPedido {...props} />
-                </>
-              )}
-            />
+          <Route
+            path="/revisarpedido"
+            name="revisarpedido"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <RevisarPedido {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/detallespago"
-              name="detallespago"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <DetallesDePago {...props} />
-                </>
-              )}
-            />
+          <Route
+            path="/detallespago"
+            name="detallespago"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <DetallesDePago {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/recetaObraSocial"
-              name="recetaObraSocial"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <RecetaConObraSocial {...props} />
-                </>
-              )}
-            />
+          <Route
+            path="/recetaObraSocial"
+            name="recetaObraSocial"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <RecetaConObraSocial {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/recetaParticular"
-              name="recetaParticular"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <RecetaParticular {...props} />
-                </>
-              )}
-            />
+          <Route
+            path="/recetaParticular"
+            name="recetaParticular"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <RecetaParticular {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/recetaPami"
-              name="recetaPami"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <RecetaPami {...props} />
-                </>
-              )}
-            />
+          <Route
+            path="/recetaPami"
+            name="recetaPami"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <RecetaPami {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/confirmacionPedido"
-              name="confirmacionPedido"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <ConfirmacionPedido {...props} />
-                </>
-              )}
-            />
-            <Route
-              path="/detalleprod"
-              name="detalleprod"
-              render={(props) => (
-                <>
-                  <NavPerfil
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <DetalleProducto {...props} />
-                </>
-              )}
-            />
-            <Route
-              path="/seleccionarfarmacia"
-              name="seleccionarfarmacia"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                    hideCategorias={true}
-                  />
-                  <FarmaciasCercanas
-                    {...props}
-                    nextPage="farmprodcat?u="
-                    filtroPerfilFarmageo={'vender_online'}
-                  />
-                  <FooterHome />
-                </>
-              )}
-            />
+          <Route
+            path="/confirmacionPedido"
+            name="confirmacionPedido"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <ConfirmacionPedido {...props} />
+              </>
+            )}
+          />
+          <Route
+            path="/detalleprod"
+            name="detalleprod"
+            render={(props) => (
+              <>
+                <NavPerfil
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <DetalleProducto {...props} />
+              </>
+            )}
+          />
+          <Route
+            path="/seleccionarfarmacia"
+            name="seleccionarfarmacia"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                  hideCategorias={true}
+                />
+                <FarmaciasCercanas
+                  {...props}
+                  nextPage="farmprodcat?u="
+                  filtroPerfilFarmageo={'vender_online'}
+                />
+                <FooterHome />
+              </>
+            )}
+          />
 
-            <Route
-              path="/buscarfarmacia"
-              name="buscarfarmacia"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                    hideCategorias={true}
-                  />
+          <Route
+            path="/buscarfarmacia"
+            name="buscarfarmacia"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                  hideCategorias={true}
+                />
 
-                  <FarmaciasCercanas {...props} nextPage="farmaciaperfil?u=" />
-                  <br />
-                  <FooterHome />
-                </>
-              )}
-            />
+                <FarmaciasCercanas {...props} nextPage="farmaciaperfil?u=" />
+                <br />
+                <FooterHome />
+              </>
+            )}
+          />
 
-            <Route
-              path="/usuarioconfig"
-              name="usuarioconfig"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <UsuarioConfig {...props} />
-                </>
-              )}
-            />
+          <Route
+            path="/usuarioconfig"
+            name="usuarioconfig"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <UsuarioConfig {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/buscarproductos"
-              name="buscarproductos"
-              render={(props) => (
-                <>
-                  {/*<NavBuscarProductos />*/}
-                  <BuscarProductos
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                    {...props}
-                  />
-                </>
-              )}
-            />
+          <Route
+            path="/buscarproductos"
+            name="buscarproductos"
+            render={(props) => (
+              <>
+                {/*<NavBuscarProductos />*/}
+                <BuscarProductos
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                  {...props}
+                />
+              </>
+            )}
+          />
 
-            <Route
-              path="/registrarfarmacia"
-              name="registrarfarmacia"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <RegistrarFarmacia {...props} />
-                </>
-              )}
-            />
+          <Route
+            path="/registrarfarmacia"
+            name="registrarfarmacia"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <RegistrarFarmacia {...props} />
+              </>
+            )}
+          />
 
-            <Route
-              path="/"
-              name="Home"
-              render={(props) => (
-                <>
-                  <NavHome
-                    modalState={modalState}
-                    setmodalState={setmodalState}
-                  />
-                  <Home {...props} />
-                </>
-              )}
-            />
-          </Switch>
-        </BrowserRouter>
+          <Route
+            path="/"
+            name="Home"
+            render={(props) => (
+              <>
+                <NavHome
+                  modalState={modalState}
+                  setmodalState={setmodalState}
+                />
+                <Home {...props} />
+              </>
+            )}
+          />
+        </Switch>
       </React.Suspense>
     </>
   );
