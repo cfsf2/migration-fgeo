6ef7c93c9b555ebdd4ae150f3c2b74077d3a6727
marcosfide <fidelibusmarcos@gmail.com>
2023-07-06 13:35:36 -0300

    apiFarmageoSql config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   image_path_server: process.env.REACT_APP_FARMAGEO_BUCKET, //incluir "/"
   apiFarmageo: process.env.REACT_APP_FARMAGEO_API,
+  apiFarmageoSql: process.env.REACT_APP_FARMAGEO_API,
   authFarmageo: "https://admin.farmageo.com.ar:8000",
   base: "https://app.farmageo.com.ar",
   //apiFarmageo: "http://localhost:3110",
