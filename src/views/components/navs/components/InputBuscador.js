846e1f0f183de4f3f51f1076778fc211bbb38866
marcosfide <fidelibusmarcos@gmail.com>
2023-07-07 17:22:13 -0300

    eliminando buscador por producto

--- src/views/components/navs/components/InputBuscador.js
+++ src/views/components/navs/components/InputBuscador.js
@@ -8,13 +8,17 @@ export const InputBuscador = (props) => {
   const [txtbusqueda, setTexto] = React.useState("");
 
   React.useEffect(() => {
-    if (farmacia) {
-      setSearch("farmacia");
-      return;
-    }
-    setSearch("producto");
-    return;
-  });
+  //   if (farmacia) {
+  //     setSearch("farmacia");
+  //     return;
+  //   }
+  //   setSearch("producto");
+  //   return;
+  // });
+    if (!farmacia) {              // Lineas nuevas para ocultar buscador por producto
+      setSearch("producto");      // Lineas nuevas para ocultar buscador por producto
+    }                             // Lineas nuevas para ocultar buscador por producto
+  }, [farmacia]);                 // Lineas nuevas para ocultar buscador por producto
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
@@ -23,6 +27,11 @@ export const InputBuscador = (props) => {
     }
     return;
   };
+
+  if (!farmacia) {  // Lineas nuevas para ocultar buscador por producto
+    return null;    // Oculta todo el componente si no es una búsqueda de farmacia
+  }                 // Lineas nuevas para ocultar buscador por producto
+
   return (
     <div className="home_input_container">
       <div className=" input_container_left">
@@ -36,7 +45,7 @@ export const InputBuscador = (props) => {
         <form className="input_container_center_input">
           <input
             type="text"
-            placeholder={`Busca tu ${farmacia ? "farmacia" : "producto"} aqui`}
+            placeholder="Busca tu farmacia aquí" //{`Busca tu ${farmacia ? "farmacia" : "producto"} aqui`}
             className="input_container_center_input"
             onChange={(e) => setTexto(e.target.value)}
             //name="txtbusqueda"
