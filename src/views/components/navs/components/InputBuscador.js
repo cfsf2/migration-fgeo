5d1ee8f0c81b07ccf0ab17bc3a539d87d4d714a0
marcosfide <fidelibusmarcos@gmail.com>
2023-07-07 17:44:02 -0300

    buscador solo por farmacia

--- src/views/components/navs/components/InputBuscador.js
+++ src/views/components/navs/components/InputBuscador.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./inputbuscador.css";
 
-export const InputBuscador = (props) => {
+//              ---->  BUSCADOR POR FARMACIA Y POR PRODUCTO   <-------
+/* export const InputBuscador = (props) => {
   const { iconFarmacia, forma1, lupa, handleSearch, farmacia } = props;
 
   const [search, setSearch] = React.useState();
@@ -45,6 +46,64 @@ export const InputBuscador = (props) => {
             onKeyPress={(e) => {
               return handleKeyDown(e);
             }}
+          />
+        </form>
+      </div>
+      <div
+        className="input_container_right"
+        style={{ cursor: "pointer" }}
+        onClick={() => handleSearch(search, txtbusqueda)}
+      >
+        <img src={lupa} alt="lupa" className="input_container_right_lupa" />
+      </div>
+    </div>
+  );
+};
+ */
+
+//              ---->  BUSCADOR SOLO POR FARMACIA  <-------
+export const InputBuscador = (props) => {
+  const { iconFarmacia, forma1, lupa, handleSearch, farmacia } = props;
+
+  const [search, setSearch] = React.useState();
+  const [txtbusqueda, setTexto] = React.useState("");
+
+  React.useEffect(() => {
+    if (!farmacia) {
+      setSearch("producto");
+    }
+  }, [farmacia]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSearch(search, txtbusqueda);
+    }
+  };
+
+  if (!farmacia) {
+    return null; // Oculta todo el componente si no es una búsqueda de farmacia
+  }
+
+  return (
+    <div className="home_input_container">
+      <div className="input_container_left">
+        <img
+          alt=""
+          src={iconFarmacia}
+          className="input_container_left_icon"
+        />
+      </div>
+      <div className="input_container_center">
+        <form className="input_container_center_input">
+          <input
+            type="text"
+            placeholder="Busca tu farmacia aquí"
+            className="input_container_center_input"
+            onChange={(e) => setTexto(e.target.value)}
+            value={txtbusqueda}
+            autoComplete="off"
+            onKeyPress={handleKeyDown}
           />
         </form>
       </div>
