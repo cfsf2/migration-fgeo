96d547c175fb051ae6605dfdb3cb9743a96733c4
Marcos Fidelibus <fidelibusmarcosgmail.com>
2025-08-18 21:44:15 +0200

    comentando contenido de BannersFinal y BAnnerInferior

--- src/views/components/home/Home.js
+++ src/views/components/home/Home.js
@@ -1,5 +1,5 @@
 import FooterHome from '../footers/FooterHome';
-// import BannerInferior from './BannerInferior';
+import BannerInferior from './BannerInferior';
 import BannerSuperior from './BannerSuperior';
 import FarmaciasCercanas from './FarmaciasCercanas/FarmaciasCercanas';
 import Suscribite from './Suscribite';
